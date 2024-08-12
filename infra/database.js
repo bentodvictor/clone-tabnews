dc4fd84f454fbaa9834efd06e1ee7d9e4519cbad
Victor Dallagnol Bento <victordallbento@hotmail.com>
2024-08-11 23:26:55 -0300

    move `.env` file to `.env.development`

--- infra/database.js
+++ infra/database.js
@@ -1,6 +1,6 @@
 import { Client } from "pg";
 
-const query = async (queryObject) => {
+async function query(queryObject) {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
@@ -8,14 +8,11 @@ const query = async (queryObject) => {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-
   await client.connect();
   const result = await client.query(queryObject);
-
   await client.end();
-
   return result;
-};
+}
 
 export default {
   query: query,
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,12 +1,9 @@
 import database from "../../../../infra/database.js";
 
-const status = async (request, response) => {
-  console.log({ request });
-  const result = await database.query("SELECT 1 + 1 as sum;");
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum");
   console.log(result.rows);
-  response.status(200).json({
-    chave: "alunos do curso.dev são pressoas acima da média",
-  });
-};
+  response.status(200).json({ chave: "são acima da média" });
+}
 
 export default status;
